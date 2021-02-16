41db00f843fbe9253fa52784d3128768af5cf768
Lioche <lionel.lamy@student.uclouvain.be>
2021-02-16 11:31:21 +0100

    Spy fix

--- db/message.js
+++ db/message.js
@@ -2,13 +2,24 @@ const db = require('./firestore');
 const {firestore} = require("firebase-admin");
 const {collections, getFormattedTimeZone} = require("../utils/utils");
 
-module.exports.saveMessage = function(msg){
+module.exports.saveMessage = async function(msg){
 
     let date = getFormattedTimeZone(0, withdate=true, timezone="Europe/Brussels");
 
     let data = {server: msg.guild.name}
-    data[msg.channel.id+".messages"] = firestore.FieldValue.arrayUnion(`(${date}) ${msg.author.username} : ${msg.content}`);
-    data[msg.channel.id+".channel"] = msg.channel.name;
     
-    db.collection(collections.messages).doc(msg.guild.id).update(data).catch(console.error);    
+    
+    let docref = await db.collection(collections.messages).doc(msg.guild.id).get()
+    
+    if(docref.exists){
+        data[msg.channel.id+".messages"] = firestore.FieldValue.arrayUnion(`(${date}) ${msg.author.username} : ${msg.content}`);
+        db.collection(collections.messages).doc(msg.guild.id).update(data).catch(console.error);   
+    }else{
+        data[msg.channel.id] = {
+            messages: firestore.FieldValue.arrayUnion(`(${date}) ${msg.author.username} : ${msg.content}`),
+            channel: msg.channel.name
+        }
+        db.collection(collections.messages).doc(msg.guild.id).set(data).catch(console.error);   
+    }
+     
 }
